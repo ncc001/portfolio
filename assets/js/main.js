d04f6220cffd4aebe77748169277adead5b0c44b
ncc <nelson.kax@gmail.com>
2022-07-07 23:02:39 -0500

    form

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 const togglem = document.querySelector('.bar__toggle');
 const heronav = document.querySelector('.hero__nav');
 const herobar = document.querySelector('.hero__barconteiner');
+const form = document.getElementById('form');
 
 window.addEventListener('scroll', function () {
 	if (window.scrollY >= 60) {
@@ -19,41 +20,30 @@ heronav.addEventListener('click', function (e) {
 	}
 });
 /* Formulario */
-// const form = document.getElementById('form');
-// form.addEventListener('submit', async (e) => {
-// 	e.preventDefault();
 
-// 	const email = 'your@email.com';
-// 	const URL_BASE = `https://formsubmit.co/${email}`;
-
-// 	const input = e.currentTarget.elements;
-// 	const formData = {
-// 		name: input.name.value,
-// 		email: input.email.value,
-// 		message: input.message.value,
-// 	};
-
-// 	const options = {
-// 		method: 'POST',
-// 		header: {
-// 			'Content-Type': 'application/json',
-// 			Accept: 'application/json',
-// 		},
-// 		body: JSON.stringify(formData),
-// 	};
-
-// 	/* con then y catch */
-// 	fetch(URL_BASE, options)
-// 		.then((response) => response.json())
-// 		.then((data) => console.log(data))
-// 		.catch((error) => console.log(error));
-
-// 	/* Con async / await */
-// 	try {
-// 		const res = await fetch(URL_BASE, options);
-// 		const data = await res.json();
-// 		console.log(data);
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// });
+form.addEventListener('submit', function (e) {
+	e.preventDefault();
+	const email = 'ncc.drive1@gmail.com';
+	const URL_BASE = `https://formsubmit.co/${email}`;
+	const input = e.currentTarget.elements;
+	const formData = {
+		name: input.name.value,
+		email: input.email.value,
+		message: input.message.value,
+	};
+	
+	window.fetch(URL_BASE, {
+			method: 'POST',
+			headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+				},
+				body: JSON.stringify(formData),
+			});
+			.then((response) => response.json())
+			.then((data) => console.log(data))
+			.catch((error) => console.log(error));
+			
+			e.currentTarget.reset();
+		});
+		
