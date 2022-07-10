833e2d8c1c1627c8b2cb00e507a51d956a8e27cb
ncc <nelson.kax@gmail.com>
2022-07-09 23:04:30 -0500

    little animation with gsap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger);
 const togglem = document.querySelector('.bar__toggle');
 const heronav = document.querySelector('.hero__nav');
 const herobar = document.querySelector('.hero__barconteiner');
@@ -50,4 +51,63 @@ form.addEventListener('submit', function (e) {
 		.catch((error) => console.log(error));
 	alert('tu mensaje se ha enviado');
 	e.currentTarget.reset();
+});
+
+const nave = document.querySelectorAll('.objectaminated');
+
+gsap.defaults({ ease: 'none', duration: 2 });
+
+ScrollTrigger.matchMedia({
+	'(min-width:769px)': function () {
+		const tl = gsap.timeline();
+
+		ScrollTrigger.create({
+			animation: tl,
+			trigger: '.animated',
+			markers: true,
+			star: 'top top',
+			end: '+=1500',
+			pin: true,
+			scrub: true,
+		});
+
+		tl.to('.objectaminated', { y: '400', duration: 5 });
+		tl.to('.objectaminated', { rotation: 45, duration: 5 });
+		tl.to('.objectaminated', { x: '500', duration: 5 });
+		tl.to('.objectaminated', { rotation: -45, duration: 5 });
+		tl.to('.objectaminated', { y: 'start', duration: 5 });
+		tl.to('.objectaminated', { rotation: 45, duration: 5 });
+		tl.to('.objectaminated', { x: '1000', duration: 5 });
+	},
+
+	'(max-width:768px': function () {
+		const tl = gsap.timeline();
+
+		ScrollTrigger.create({
+			animation: tl,
+			trigger: '.animated',
+			markers: true,
+			star: 'top top',
+			end: '+=2000',
+			pin: true,
+			scrub: true,
+		});
+
+		tl.to('.objectaminated', { y: '100', duration: 15 });
+		tl.to('.objectaminated', { rotation: 45, duration: 15 });
+		tl.to('.objectaminated', { x: '300', duration: 15 });
+		tl.to('.objectaminated', { rotation: -45, duration: 15 });
+		tl.to('.objectaminated', { y: '-40', duration: 15 });
+		tl.to('.objectaminated', { rotation: -120, duration: 15 });
+		tl.to('.objectaminated', { x: 'start', duration: 15 });
+		tl.to('.objectaminated', { rotation: -220, duration: 15 });
+		tl.to('.objectaminated', { y: '760', duration: 125 });
+		tl.to('.objectaminated', { rotation: 420, duration: 15 });
+		tl.to('.objectaminated', { x: '330', duration: 15 });
+		tl.to('.objectaminated', { rotation: -40, duration: 15 });
+		tl.to('.objectaminated', { y: '245', duration: 15 });
+		tl.to('.objectaminated', { rotation: -95, duration: 15 });
+		tl.to('.objectaminated', { x: '255', duration: 15 });
+		tl.to('.objectaminated', { rotation: 360, duration: 125 });
+	},
 });
